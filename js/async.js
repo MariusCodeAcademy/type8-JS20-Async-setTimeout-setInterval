e10bd22d5e6638c39e21162f8d9208df4867e06e
Marius <marius@example.com>
2022-03-22 11:29:13 +0200

    promisifying setTimeout

--- js/async.js
+++ js/async.js
@@ -1,16 +1,24 @@
 function fn1() {
   console.log('Number One 1');
 }
-function fn2(cb) {
-  setTimeout(() => {
-    console.log('Number Two 2');
-    cb();
-  }, 1500);
+function fn2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Number Two 2');
+      resolve();
+    }, 1500);
+  });
 }
 function fn3() {
   console.log('Number Three 3');
 }
+function fn4() {
+  console.log('Number Four 4');
+}
 // debugger;
 fn1();
-fn2(fn3);
+fn2().then(() => {
+  fn3();
+  fn4();
+});
 // fn3();
